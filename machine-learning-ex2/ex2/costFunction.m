d764e2090efc4703790d2132b3791e417ad0fe70
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-09-02 17:02:50 +0300

    Logistic Regression Cost & Gradient.

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,9 +20,12 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h = sigmoid(X * theta); % size(X) = [100 3]; size(theta) = [3 1];
+term1 = (-y) .* log(h);
+term2 = (1 - y) .* log(1 - h);
+J = sum(term1 - term2) / m;
 
-
-
+grad = (1 / m) * (X' * (h - y)); %' why???
 
 
 
