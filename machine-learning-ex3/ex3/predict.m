bc11c45092373020f67957704c90ec654de086ac
Igor Moiseyenko <imoiseyenko93@gmail.com>
2017-09-12 17:30:30 +0300

    Neural Network Prediction Function.

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,10 +21,25 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% 25 units in the hidden layer (2nd)
+% 10 units (= num_labels) in the output layer (3rd)
+% size(Theta1) = [25 401]
+% size(Theta2) = [10 26]
 
+X = [ones(m, 1) X]; % size(X) = [m 401]
+z1 = X * Theta1'; %' size(z1) = [m 25]
+a1 = sigmoid(z1); % size(a1) = [m 25]
+a1 = [ones(m, 1) a1]; % size(a1) = [m 26]
+z2 = a1 * Theta2'; %' size(z2) = [m 10];
+a2 = sigmoid(z2);
 
-
-
+% from help max:
+%   -- max (X, [], DIM)
+%   If the optional third argument DIM is present then operate along
+%   this dimension.  In this case the second argument is ignored and
+%   should be set to the empty matrix.
+[p_max p_max_index] = max(a2, [], 2);
+p = p_max_index;
 
 
 
